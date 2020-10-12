33b8763d9caa75c6a3d829bb52f75babf520e63b
Pierre Fleury <pierre.a.fleury@gmail.com>
2020-10-12 17:52:21 +0200

    update 12.10.2020 17.50

--- Classes/Actions.java
+++ Classes/Actions.java
@@ -3,71 +3,112 @@ import lejos.hardware.Sound;
 import lejos.hardware.motor.*;
 import lejos.hardware.port.*;
 import lejos.utility.Delay;
+
+//findBug Java & checkJava pour corriger
 /**
  * @author Utilisateur
  *
  */
 public class Actions {
+	String AttRobot="Attention Robot !";
+	String AttPalet="Attention Palet !";
+	String AttMur="Attention Mur !";
+	// create two motor objects to control the motors.
+	UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
+	UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
+	UnregulatedMotor motorC = new UnregulatedMotor(MotorPort.C);
 
 	public Actions() {
 		// TODO Auto-generated constructor stub
 	}
 
+	//final ? surcharge autorisée? 
 
 	/**
-	 * permet d'avancer en activant les deux servomoteurs
+	 * Méthode qui permet d'avancer en activant les deux servomoteurs
+	 * pq la méthode s'arrête
 	 */
-	public void avancer () {
-		System.out.println("Drive Forward\nand Stop\n");
-        System.out.println("Press any key to start");
-
-        Button.LEDPattern(4);     // flash green led and
-        Sound.beepSequenceUp();   // make sound when ready.
-
-        Button.waitForAnyPress();
-
-        // create two motor objects to control the motors.
-        UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
-        UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
-
-        // set motors to 50% power.
-        motorA.setPower(50);
-        motorB.setPower(50);
-
-        // wait 2 seconds.
-        Delay.msDelay(2000);
-
-        // stop motors with brakes on. 
-        motorA.stop();
-        motorB.stop();
-
-        // free up motor resources. 
-        motorA.close(); 
-        motorB.close();
- 
-        Sound.beepSequence(); // we are done.
+	public String avancer (int cm) {
+		activerServomoteurs();
+		Delay.msDelay(cm);// utiliser un paramètre à la place de 2000
+		if(distance<paramètreFinalADefinir) {
+			motorA.stop(); //motor A s'arrête
+			motorB.stop(); //motor A s'arrête
+			if() {//si robot
+				//faire action X
+				return AttRobot;
+			}
+			else if( ) {//si palet
+				//faire action Y
+				return AttPalet;
+			}
+			else if (){//si mur
+				//faire action Z
+				return AttRobot;
+			}
+			else () {//si ligne blanche
+				//faire action B
+				return "Ligne blanche";
+			}
+		}
+		return null;
 	}
 
-	public void reculer () {
 
+	public void reculer () {//reculer
+		motorA.backward();
+		motorB.backward();
+		activerServomoteurs();
+		Delay.msDelay(500);
+		arreter();
 	}
 
-	public void avancerUneRoue () {
-
+	public void demiTour () {//faire demi-tour
+		motorA.forward();
+		motorB.backward();
+		activerServomoteurs();
+		Delay.msDelay(500);//500 est à remplacer par une constante à définir en test
+		arreter();
 	}
 
-	public void reculerUneRoue () {
-
+	public void avancerUneRoue (char gd, int angle) {//tourne à gauche
+		if(gd=='d') {
+			motorA.backward();
+			motorB.forward();
+			activerServomoteurs();
+			Delay.msDelay(500);//500 est à remplacer par une valeur à définir en test
+			arreter();
+		}
+		else if(gd=='g') {
+			motorA.forward();
+			motorB.backward();
+			activerServomoteurs();
+			Delay.msDelay(500);//500 est à remplacer par une valeur à définir en test
+			arreter();
+		}
 	}
-	public void tourner () {
 
+	/**
+	 * Méthode qui doit tourner le robot d'un angle défini à gauche ou à droite
+	 * @param gd désigne la gauche ou la droite
+	 * @param angle est l'angle dont le robot doit tourner, l'angle est déjà converti en temps
+	 */
+	public void tourner (char gd,int angle) {
+		avancerUneRoue(gd,angle);
+		//check le SonicSensor pour savoir si on peut avancer
+		//check le ColorSensor pour savoir où on est à peu près
 	}
+
 	public void arreter () {
-
+		// stop motors with brakes on. 
+		motorA.stop();
+		motorB.stop();
 	}
 
 	public void activerServomoteurs () {
-
+		// set motors to 50% power.
+		motorA.setPower(50);
+		motorB.setPower(50);
 	}
 
 	public void esquiverRobot (){
@@ -75,13 +116,21 @@ public class Actions {
 	}
 
 	public void reconnaitre () {
-
+		if()
 	}
+	
 	public void ouvrirPinces () {
-
+		motorC.backward();
+		motorC.setPower(100);
+		Delay.msDelay(1000);
+		motorC.close();
 	}
+	
 	public void fermerPinces () {
-
+		motorC.forward();
+		motorC.setPower(100);
+		Delay.msDelay(1000);
+		motorC.close();
 	}
 	public void recupererPalet () {
 
@@ -89,9 +138,9 @@ public class Actions {
 	public void trouverLigne () {
 
 	}
-	
-	 public void test () {
-		 
-	 }
+
+	public void test () {
+
+	}
 
 }
