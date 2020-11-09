7d35affde40ba8a8328b7058bb78f19ed1d609b1
celia <celiajor@gmail.com>
2020-11-09 13:47:30 +0100

    Base Code Capteurs

---
+++ Classes/Base Code Final/Capteurs.java
@@ -0,0 +1,44 @@
+package ia;
+import lejos.hardware.port.Port;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+
+/**
+ * @author Vincent
+ * La classe gerant le capteur de pression du robot
+ */
+public class Capteurs extends EV3TouchSensor{
+	EV3UltrasonicSensor port2 = new EV3UltrasonicSensor(SensorPort.S2);
+	
+	/*
+	 */
+	public Capteurs(Port port,EV3UltrasonicSensor port2){
+		super(port);
+		this.port2=port2;
+	}
+	
+	/**
+	 * @author Vincent
+	 * @return true si le capteur est en position basse, false sinon
+	 */
+	public boolean isPressed(){
+		float[] sample = new float[1];
+		fetchSample(sample, 0);
+		return sample[0] != 0;
+	}
+
+	/**
+	 * @author Vincent
+	 * @return la distance en metre entre l'obstacle present devant le robot et le robot
+	 */
+	public float getDistance() {
+		SampleProvider distance = this.getMode("Distance");
+		float[] sample = new float[distance.sampleSize()];
+		distance.fetchSample(sample, 0);
+		return sample[0];
+	}
+
+	
+}
