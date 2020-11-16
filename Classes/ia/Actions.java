35960c55e5842df2915871ee7cfbc2550a40eb94
Marguerite Kosaréva <m.kosareva@g.nsu.ru>
2020-11-16 23:01:48 +0700

    modifier la classe Action

--- Classes/ia/Actions.java
+++ Classes/ia/Actions.java
@@ -1,6 +1,3 @@
-package ia;
-
-
 import lejos.hardware.Sound;
 import lejos.hardware.motor.*;
 import lejos.hardware.port.*;
@@ -10,105 +7,188 @@ import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
 import lejos.hardware.Button;
+import lejos.hardware.motor.*;
+import lejos.hardware.port.*;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.utility.Delay;
+import lejos.hardware.Button;
+import lejos.robotics.RegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 /**
  * @author Utilisateur
  *
  */
-public class Actions {
-
+public class Actions extends Capteurs{
+	
 	private Chassis chassis;
 	private MovePilot mp;
 	private EV3LargeRegulatedMotor moteurGauche;
 	private EV3LargeRegulatedMotor moteurDroit;
 	private EV3MediumRegulatedMotor moteurPince;
-	String AttRobot="Attention Robot !";
-	String AttPalet="Attention Palet !";
-	String AttMur="Attention Mur !";
-	
-	public Actions (Port portA, Port portB, Port portC) {
-		moteurGauche = new EV3LargeRegulatedMotor(portB);
-		moteurPince = new EV3MediumRegulatedMotor(portC);
-		moteurDroit = new EV3LargeRegulatedMotor(portA);
-		Wheel wheel1 = WheeledChassis.modelWheel(moteurGauche, 56).offset(-60);
-		Wheel wheel2 = WheeledChassis.modelWheel(moteurDroit,  56).offset( 60);
-		chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
-		mp = new MovePilot(chassis);
-	} 
 
-	/**
-	 * MÈthode qui permet d'avancer en activant les deux servomoteurs
-	 */
-	public void avancer() {
-		mp.forward();
-		Delay.msDelay(2000);
-	}
-
-	public void reculer () {
-		mp.backward();
-		Delay.msDelay(2000);
-	}
-
-	public void demiTour () {
 		
-		Delay.msDelay(500);//500 est ‡ remplacer par une constante ‡ dÈfinir en test
-		arreter();
-	}
+			private static EV3UltrasonicSensor port2;
+			Wheel wheel1 = WheeledChassis.modelWheel(moteurGauche, 56).offset(-60);
+			Wheel wheel2 = WheeledChassis.modelWheel(moteurDroit,  56).offset( 60);
+			chassis = new WheeledChassis(new Wheel[] { wheel1, wheel2 }, WheeledChassis.TYPE_DIFFERENTIAL);
+			mp = new MovePilot(chassis);
+			String AttRobot="Attention Robot !";
+			String AttPalet="Attention Palet !";
+			String AttMur="Attention Mur !";
+			// create two motor objects to control the motors.
+			UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
+			UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
+			UnregulatedMotor motorC = new UnregulatedMotor(MotorPort.C);
+			RegulatedMotor motorAr = new EV3LargeRegulatedMotor(MotorPort.A);
+			RegulatedMotor motorBr = new EV3LargeRegulatedMotor(MotorPort.B);
 
-	public void tourner90 (char gd) {//tourne ‡ gauche
-		if(gd=='d') {
-			moteurGauche.forward();
-			moteurDroit.stop();
-			Delay.msDelay(1000);//500 est ‡ remplacer par une valeur ‡ dÈfinir en test
-		}
-		else if(gd=='g') {
+			public Actions() {
+				super(null, port2);
+				// TODO Auto-generated constructor stub
+			}
+
+			//final ? surcharge autoris√©e? 
+
+			/**
+			 * M√©thode qui permet d'avancer en activant les deux servomoteurs
+			 * pq la m√©thode s'arr√™te
+			 */
+			public void avancer() {
+				mp.forward();
+				Delay.msDelay(2000);
+			}
+
+			public void reculer () {//reculer
+				mp.backward();
+				Delay.msDelay(2000);
+			}
+
+			public void demiTour () {//faire demi-tour
+				Delay.msDelay(500);//500 est √† remplacer par une constante √† d√©finir en test
+				arreter();
+			}
+
+			public void tourner90 (char gd) {//tourne √† gauche
+				if(gd=='d') {
+					motorA.backward();
+					motorB.forward();
+					activerServomoteurs();
+					Delay.msDelay(500);//500 est √† remplacer par une valeur √† d√©finir en test
+					arreter();
+				}
+				else if(gd=='g') {
+					motorA.forward();
+					motorB.backward();
+					activerServomoteurs();
+					Delay.msDelay(500);//500 est √† remplacer par une valeur √† d√©finir en test
+					arreter();
+				}
+			}
+
+			/**
+			 * M√©thode qui doit tourner le robot d'un angle d√©fini √† gauche ou √† droite
+			 * @param gd d√©signe la gauche ou la droite
+			 * @param angle est l'angle dont le robot doit tourner, l'angle est d√©j√† converti en temps
+			 */
+			public void tourner (char gd,int angle) {
+				if(gd=='d') {
+					motorAr.startSynchronization();
+					motorAr.rotate(angle);
+					motorBr.rotate(angle);
+					motorAr.endSynchronization();
+					activerServomoteurs();
+					Delay.msDelay(500);//500 est √† remplacer par une valeur √† d√©finir en test
+					arreter();
+				}
+				else if(gd=='g') {
+					motorBr.startSynchronization();
+					motorBr.rotate(angle);
+					motorAr.rotate(angle);
+					motorBr.endSynchronization();
+					activerServomoteurs();
+					Delay.msDelay(500);//500 est √† remplacer par une valeur √† d√©finir en test
+					arreter();
+				}
+				//avancerUneRoue(gd,angle);
+				//check le SonicSensor pour savoir si on peut avancer
+				//check le ColorSensor pour savoir o√π on est √† peu pr√®s
+			}
+
+			public void arreter () {
+				// stop motors with brakes on. 
+				motorA.stop();
+				motorB.stop();
+			}
+
+			public void activerServomoteurs () {
+				// set motors to 50% power.
+				motorA.setPower(50);
+				motorB.setPower(50);
+			}
+
+			public void esquiverRobot (){
+
+			}
+
+			public void reconnaitre () {
+				/*if(isPressed()) {
+					System.out.println(AttPalet);
+				}
+				if(getDistance() < 5) {  //5 est √† remplacer par une valeur √† d√©finir en test
+					System.out.println(AttMur);
+				}
+				if(sensorSonor()){
+					System.out.println(AttRobot); }*/
+			}
+
+			public void ouvrirPinces () {
+				motorC.backward();
+				motorC.setPower(100);
+				Delay.msDelay(1000);
+				motorC.close();
+			}
+
+			public void fermerPinces () {
+				motorC.forward();
+				motorC.setPower(100);
+				Delay.msDelay(1000);
+				motorC.close();
+			}
 			
-			Delay.msDelay(500);//500 est ‡ remplacer par une valeur ‡ dÈfinir en test
-			arreter();
-		}
-	}
+			public void recupererPalet () {
+				for(int i = 15; i > 10; i --) { //15 et 10 sont √† remplacer par une valeur √† d√©finir en test
+					if(getDistance() == i) {
+						avancer();
+						if(isPressed()) {
+							fermerPinces();
+						}
+					}
+					else {
+						tourner('g', 10);
+						if(getDistance() == i) {
+							avancer();
+							if(isPressed()) {
+								fermerPinces();
+							}
+						}
+						else {
+							tourner('d', 20);
+						}
+					}
+				}
+			}
+			
+			public void deposerPalet () {
+				if(CapteurCouleur() == "blanc") {
+					ouvrirPinces();
+					tourner('g', 180);
+				}
+			}
 
-	/**
-	 * MÈthode qui doit tourner le robot d'un angle dÈfini ‡ gauche ou ‡ droite
-	 * @param gd dÈsigne la gauche ou la droite
-	 * @param angle est l'angle dont le robot doit tourner, l'angle est dÈj‡ converti en temps
-	 */
-	public void tourner (char gd,int angle) {
-		//avancerUneRoue(gd,angle);
-		//check le SonicSensor pour savoir si on peut avancer
-		//check le ColorSensor pour savoir o˘ on est ‡ peu prËs
-	}
+			public void test () {
 
-	public void arreter () {
+			}
+
 		
-	}
-
-	public void esquiverRobot (){
 
 	}
-
-	public void reconnaitre () {
-		//if()
-	}
-
-	public void ouvrirPinces () {
-		
-	}
-
-	public void fermerPinces () {
-		
-	}
-	public void recupererPalet () {
-
-	}
-
-	public void trouverLigne () {
-
-	}
-
-	public void test () {
-
-	}
-
-
-
-}
