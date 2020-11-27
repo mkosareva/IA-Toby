c39bcb81cde54e49f84c3f7a2bba3cfe29321b2e
celia <celiajor@gmail.com>
2020-11-27 13:03:31 +0100

    Modif Banane et code de quelques methodes dans Actions

--- Classes/ia/Actions.java
+++ Classes/ia/Actions.java
@@ -4,6 +4,8 @@ package ia;
 import lejos.hardware.Sound;
 import lejos.hardware.motor.*;
 import lejos.hardware.port.*;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
@@ -24,6 +26,11 @@ public class Actions {
 	String AttRobot="Attention Robot !";
 	String AttPalet="Attention Palet !";
 	String AttMur="Attention Mur !";
+	public  EV3UltrasonicSensor distance ;
+	private SampleProvider ecart = distance.getMode("Distance");
+	private float[] tab;
+	private boolean pincesOuverte =true;
+	Capteurs c= new Capteurs();
 
 	public Actions (Port portA, Port portB, Port portC) {
 		moteurGauche = new EV3LargeRegulatedMotor(portB);
@@ -54,7 +61,7 @@ public class Actions {
 	public void reculer (int vitesse, int distance) { 
 		avancer(vitesse, -distance);
 	}
-	
+
 	/*
 	 * Mйthode qui permet au robot de s'orienter diffйrement selon l'angle
 	 * rentrй en paramиtre
@@ -72,52 +79,90 @@ public class Actions {
 	}
 
 	public void esquiverRobot (){
-		
+
 	}
 
 	public void reconnaitre () {
-		
-	}
+		/* 
+		 * valeur a mesurer 
+		 * mise au pif ici ATTENTION 
+		 */
+		while(pincesOuverte) {
+			if((getDistance()<200 && getDistance()> 20) && !c.isPressed()) {
+				avancer(400,10);
+			}
 
-	public void ouvrirPinces () {
-		moteurPince.rotate((int)moteurPince.getMaxSpeed());
-	}
-
-	public void fermerPinces () {
-		moteurPince.rotate(-(int)moteurPince.getMaxSpeed());
-	}
-	
-	public void recupererPalet () {
-		for(int i = 15; i > 10; i --) { //15 et 10 sont à remplacer par une valeur à définir en test
-					if(getDistance() == i) {
-						avancer();
-						if(isPressed()) {
-							fermerPinces();
-						}
-					}
-					else {
-						tourner('g', 10);
-						if(getDistance() == i) {
-							avancer();
-							if(isPressed()) {
-								fermerPinces();
-							}
-						}
-						else {
-							tourner('d', 20);
-						}
-					}
-				}
-	}
-
-	public void deposerPalet () {
-				if(CapteurCouleur() == "blanc") {
-					ouvrirPinces();
-					orienter(180);
+			if(getDistance()<20 && ! c.isPressed()) {
+				/* 
+				 * on avance encore un peu si jamais 
+				 * le bouton est presser comme ca 
+				 */
+				avancer(200,5); 
+				while(getDistance()<200 && getDistance()> 20) { 
+					/* 
+					 * on tourne doucement jusqu'a trouver un 
+					 * object (techniquement palet) proche 
+					 * de nous 
+					 */
+					orienter(10);
 				}
 			}
 
-	
+			if(c.isPressed()) {
+				fermerPinces();
+			}
+		}
+	}
+	private float getDistance() {
+		ecart = distance.getDistanceMode();
+		ecart.fetchSample(tab, 0);
+		return tab[0];
+	}
+
+
+	public boolean ouvrirPinces () {
+		moteurPince.rotate((int)moteurPince.getMaxSpeed());
+		pincesOuverte=true;
+		return pincesOuverte; 
+	}
+
+	public boolean fermerPinces () {
+		moteurPince.rotate(-(int)moteurPince.getMaxSpeed());
+		pincesOuverte=false;
+		return pincesOuverte;
+	}
+
+	public void recupererPalet () {
+		for(int i = 15; i > 10; i --) { //15 et 10 sont à remplacer par une valeur à définir en test
+			if(getDistance() == i) {
+				avancer();
+				if(c.isPressed()) {
+					fermerPinces();
+				}
+			}
+			else {
+				tourner('g', 10);
+				if(getDistance() == i) {
+					avancer();
+					if(c.isPressed()) {
+						fermerPinces();
+					}
+				}
+				else {
+					tourner('d', 20);
+				}
+			}
+		}
+	}
+
+	public void deposerPalet () {
+		if(c.CapteurCouleur() == "blanc") {
+			ouvrirPinces();
+			orienter(180);
+		}
+	}
+
+
 	public void get2PremierPaletPourUnAvantageDeFou() {
 
 		/**
@@ -128,7 +173,7 @@ public class Actions {
 		while(!port.isPressed()) {
 			avancer(100,100);
 		}
-		*/
+		 */
 		avancer(400,1000);
 		arreter();
 		fermerPinces();
@@ -140,7 +185,7 @@ public class Actions {
 		/*while(couleur!="blanc") {
 			avancer();
 		}
-		*/
+		 */
 		arreter();
 		ouvrirPinces();
 		avancer(300,-200);
--- Classes/ia/Banane.java
+++ Classes/ia/Banane.java
@@ -3,39 +3,41 @@ package ia;
 import lejos.hardware.Button;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
-import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.Color;
 import lejos.robotics.ColorDetector;
-import lejos.robotics.chassis.Chassis;
+/*import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.MovePilot;
-import lejos.utility.Delay;
+import lejos.utility.Delay;*/
 
 public class Banane {
-	
+
 	public static void main(String []a){
 		Actions test=new Actions (MotorPort.A, MotorPort.B, MotorPort.C);
 		Capteurs capteur=new Capteurs();
-		//Capteurs test1=new Capteurs();
-		
-		//test.orienter(90); //=> ok
-		//test.avancer(100, 100); //=> ok 
-		//test.reculer(100, 100); //=> ok 
-		//test.fermerPinces();
-		//test.ouvrirPinces();
-		//System.out.println("salut");
-		test.get2PremierPaletPourUnAvantageDeFou();
-		/*if(!capteur.isPressed()) {
+		Button.waitForAnyPress();
+		while(!Button.ESCAPE.isUp()) { // ou isDown ?
+			//Capteurs test1=new Capteurs();
+
+			//test.orienter(90); //=> ok
+			//test.avancer(100, 100); //=> ok 
+			//test.reculer(100, 100); //=> ok 
+			//test.fermerPinces();
+			//test.ouvrirPinces();
+			//System.out.println("salut");
+			test.get2PremierPaletPourUnAvantageDeFou();
+			/*if(!capteur.isPressed()) {
 			test.avancer(400,1000);
 		}
-		*/
-		//test.avancer(400, 1000);
-		//test.orienter(90);
-		//test.avancer(400, 2000);
+			 */
+			//test.avancer(400, 1000);
+			//test.orienter(90);
+			//test.avancer(400, 2000);
+		}
 	}
 }
